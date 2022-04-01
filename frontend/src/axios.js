4a8ec16c168d955d24ee27efe0ffe12602511dff
jasperchan5 <jscnn51011@gmail.com>
2022-04-02 00:07:47 +0800

    a

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = window.location.protocol + '//' + window.location.hostname + ':80';
+const baseURL = 'http://localhost:80';
 console.log(baseURL);
 const instance = axios.create({
   baseURL: baseURL
