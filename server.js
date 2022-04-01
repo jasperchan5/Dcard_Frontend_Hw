432f13a8e8b30636a2bc04bf29007fff5e4e4450
jasperchan5 <jscnn51011@gmail.com>
2022-04-02 01:56:28 +0800

    a

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.get("/*", (_, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
+console.log(path.join(__dirname, "frontend", "build", "index.html"));
 
 // define server
 const port = process.env.PORT || 80;
