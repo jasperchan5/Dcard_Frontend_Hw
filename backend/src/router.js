bb37e870a88fb5066fc8bbf68d46f5618f9255b9
jasperchan5 <jscnn51011@gmail.com>
2022-04-02 05:12:47 +0800

    dep

--- backend/src/router.js
+++ backend/src/router.js
@@ -6,18 +6,28 @@ const router = express.Router();
 dotenv.config();
 
 router.get('/getRepos', async(req,res) => {
-    const userName = req.query.username;
-    console.log(`Fetching ${userName}'s repo list!`);
-    const repoList = await octokit.request(`GET /users/${userName}/repos?per_page=100`);
-    console.log("Repo count:",repoList.data.length);
-    res.status(200).send({repoList: repoList});
+    try{
+        const userName = req.query.username;
+        console.log(`Fetching ${userName}'s repo list!`);
+        const repoList = await octokit.request(`GET /users/${userName}/repos?per_page=100`);
+        console.log("Repo count:",repoList.data.length);
+        res.status(200).send({repoList: repoList});
+    }
+    catch(e){
+        res.status(404).send(e);
+    }
 })
 
 router.get('/getSingleRepo', async(req,res) => {
-    const userName = req.query.username, repoName = req.query.reponame;
-    console.log(`Fetching information of ${userName}'s repo ${repoName}!`);
-    const repoInfo = await octokit.request(`GET /repos/${userName}/${repoName}`);
-    res.status(200).send({repoInfo: repoInfo});
+    try{
+        const userName = req.query.username, repoName = req.query.reponame;
+        console.log(`Fetching information of ${userName}'s repo ${repoName}!`);
+        const repoInfo = await octokit.request(`GET /repos/${userName}/${repoName}`);
+        res.status(200).send({repoInfo: repoInfo});
+    }
+    catch(e){
+        res.status(404).send(e);
+    }
 })
 
 export default router;
