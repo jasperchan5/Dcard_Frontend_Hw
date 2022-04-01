169d2adbb97e7b5ccc7b7479ee3c951282df0a78
jasperchan5 <jscnn51011@gmail.com>
2022-04-01 16:27:44 +0800

    server

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
-
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://jasper-repo-viewer.herokuapp.com:80'
+                                                      : 'http://localhost:80';
+console.log(baseURL);
 const instance = axios.create({
-  baseURL: 'https://jasper-repo-viewer.herokuapp.com:80',
+  baseURL: baseURL,
 });
 
 export default instance;
