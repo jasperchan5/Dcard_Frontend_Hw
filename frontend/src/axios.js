91fcd1f56052cfa480efad9ec58d73ab17ae8519
jasperchan5 <jscnn51011@gmail.com>
2022-04-02 05:03:43 +0800

    dep

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv-defaults';
-dotenv.config();
 
-const PORT = process.env.PORT;
-console.log(PORT);
-const baseURL = 'https://jasper-repo-viewer.herokuapp.com:' + PORT;
-console.log(baseURL);
 const instance = axios.create({
-  baseURL: baseURL
+  baseURL: ''
 });
 
 export default instance;
