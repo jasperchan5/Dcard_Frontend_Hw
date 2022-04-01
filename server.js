5ac37ff68b3c483f1dba9fade9b4866dec579e6e
jasperchan5 <jscnn51011@gmail.com>
2022-04-02 02:14:14 +0800

    a

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(bodyParser.json());
 app.use('/', router);
 app.use('/getRepos', router);
 app.use('/getSingleRepo', router);
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", (_, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 console.log(path.join(__dirname, "frontend", "build", "index.html"));
 
